e20b5202e3f2ddc19bd2f4fa1af6b6d4375f40a5
Howard Huang <h.huang65@gmail.com>
2022-01-06 14:33:01 -0800

    Change font_size for macOS

--- config.lua
+++ config.lua
@@ -1,9 +1,27 @@
 local wezterm = require 'wezterm'
 
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
+local font_size = 12
+local os_name = os.capture 'uname'
+
+if os_name == "Darwin" then
+  font_size = 16
+end
+
 return {
   default_cursor_style = "BlinkingBlock",
   font = wezterm.font("JetBrainsMono Nerd Font"),
-  font_size = 12,
+  font_size = font_size,
   enable_tab_bar = false,
   enable_wayland = true,
   window_decorations = "RESIZE",
