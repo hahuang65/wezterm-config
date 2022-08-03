bb9a6ab9444cecb47bba03745b3f70750e6eed67
Howard Huang <hao@hwrd.me>
2022-08-03 10:07:25 -0700

    Install lua-based colors

--- install.sh
+++ install.sh
@@ -9,3 +9,8 @@ for color in colors/*.toml
 do
     ln -sf "${PWD}/${color}" "${HOME}/.config/wezterm/colors/"
 done
+
+for color in colors/*.lua
+do
+    ln -sf "${PWD}/${color}" "${HOME}/.config/wezterm/colors/"
+done
