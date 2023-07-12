8a19ebb1b8ce700ae0600a56525d6f30df954bed
Howard Huang <hao@hwrd.me>
2023-07-12 10:00:01 -0700

    Change to CommitMono

--- config.lua
+++ config.lua
@@ -40,7 +40,7 @@ for _, v in ipairs(hyperlink_rules) do
 end
 
 local font_size = 16
-local primary_font = "Iosevka"
+local primary_font = "CommitMono"
 if wezterm.target_triple == "x86_64-apple-darwin" then
   -- Custom options for macOS
 elseif wezterm.hostname() == "ilum" then
